3af5bc8db533587d5317cad9dfbb5ec6ef8a57be
greeyeer49 <gabriel-revigliono@outlook.com>
2021-07-01 11:02:31 -0300

    Resolución ejercicio

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -91,10 +91,13 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var respuesta=0;
-  for(i = 0; i < resultadosTest.length; i++) {respuesta = respuesta + resultadosTest[i];}
-  respuesta = respuesta / resultadosTest.length;
-  return respuesta;
+  // var respuesta=0;
+  // for(i = 0; i < resultadosTest.length; i++) {respuesta = respuesta + resultadosTest[i];}
+  // respuesta = respuesta / resultadosTest.length;
+  // return respuesta;
+  var respuesta = agregarNumeros(resultadosTest);
+  var promedio = respuesta / resultadosTest.length
+  return promedio
 }
 
 
@@ -112,11 +115,11 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  var respuesta = 1;
-  for(i = 0; i < arguments.length; i++) 
-  {if (arguments[i]==='') 
-    {respuesta = 0}; 
-    respuesta = respuesta * arguments[i];}
+  if (arguments.length === 0) {return 0;} 
+  if (arguments.length === 1) {return arguments[0];}
+  if (arguments.length > 1)
+  {var respuesta = 1;
+  for(i = 0; i < arguments.length; i++) {respuesta = respuesta * arguments[i];}}
   return respuesta;
 }
 
@@ -145,7 +148,8 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var respuesta = n.toString();
+  if (respuesta.charAt(0) == 9) {return true}; {return false}
 }
 
 
@@ -153,7 +157,11 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var respuesta = arreglo[0];
+  for(i=1;i<arreglo.length;i++)
+  {if (respuesta !== arreglo[i]) {break;} respuesta = arreglo[i];
+return true}
+return false
 } 
 
 
@@ -162,6 +170,11 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var meses = [];
+  for(i=0;i<array.length;i++)
+  {if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {meses.push(array[i])}; ;}
+  if (meses.length < 3) {return "No se encontraron los meses pedidos"};
+  return meses;
 }
 
 
@@ -169,7 +182,7 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  var respuesta='';
+  var respuesta = [];
   for(i = 0; i < array.length; i++) 
   {if (array[i]>100) 
     {respuesta.push(array[i])}; ;}
@@ -185,6 +198,12 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  respuesta = [];
+  for(i = 0; i < 10; i++)
+  {numero = numero + 2;
+  if (i === numero) {return "Se interrumpió la ejecución"};
+  respuesta.push(numero)}
+  return respuesta;
 }
 
 
@@ -195,6 +214,10 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  respuesta = [];
+  for(i = 0; i < 10; i++)
+  {if (i === 5) {continue}; {numero = numero + 2; respuesta.push(numero)};}
+  return respuesta;
 }
 
 
